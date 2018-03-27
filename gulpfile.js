3f409223d5f7a7424e9f9011405c050a6e0085cf
Angus.yg <gael.clair@gmail.com>
2018-03-28 00:38:06 +0200

    Modification of gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -4,181 +4,125 @@ const pump = require('pump');
 const plugins = require('gulp-load-plugins')();
 const gutil = require('gulp-util');
 const uglify = require('gulp-uglify-es').default;
-const fs = require('fs');
+const runSequence = require('run-sequence');
 
 const libJs = [
-  'node_modules/jquery/dist/jquery.min.js',
-  'node_modules/angular/angular.min.js',
-  'node_modules/angular-animate/angular-animate.min.js',
-  'node_modules/angular-messages/angular-messages.min.js',
-  'node_modules/@uirouter/angularjs/release/angular-ui-router.min.js',
-  'node_modules/angular-storage/dist/angular-storage.min.js',
-  'node_modules/angular-translate/dist/angular-translate.min.js',
-  'node_modules/angular-translate-loader-partial/angular-translate-loader-partial.min.js',
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/angular/angular.js',
+  'node_modules/angular-animate/angular-animate.js',
+  'node_modules/angular-messages/angular-messages.js',
+  'node_modules/@uirouter/angularjs/release/angular-ui-router.js',
+  'node_modules/angular-storage/dist/angular-storage.js',
+  'node_modules/angular-translate/dist/angular-translate.js',
+  'node_modules/angular-translate-loader-partial/angular-translate-loader-partial.js',
   'node_modules/ui-bootstrap4/dist/ui-bootstrap-tpls.js',
 ];
-const libCss = ['node_modules/bootstrap/dist/css/bootstrap.min.css'];
-const sourceJs = [
+const localJs = [
   'src/app/**/*.module.js',
   'src/app/**/*.js',
   '!**/*gulpfile.js',
   '!**/*.eslintrc.js'
 ];
+const sourceJs = libJs.concat(localJs);
 const sourceCss = [
   'src/styles/**/*.sass',
   'src/styles/**/*.scss'
 ];
-const indexHtml = './dist/index.html';
-const sourceHtml = ['tests/**/*.html'];
-const destinationJs = './dist';
-const destinationCss = './dist';
-const destinationHtml = './dist';
+const sourceHtml = ['src/html/**/*'];
+const dest = 'web';
+const destinationJs = `${dest}/js`;
+const destinationCss = `${dest}/stylesheets`;
+const destinationHtml = `${dest}`;
 const finalJs = 'frontend.core.js';
-const finalLibJs = 'lib.min.js';
-const esLintJs = ['src/app/**/*.js'];
-const cleanJs = [`${destinationJs}/**/*.js`];
-const cleanCss = [
-  `${destinationCss}/**/*.css`,
-  `${destinationCss}/**/*.map`,
+const lintJs = [
+  'src/app/**/*.js',
+  '!**/gulpfile.js',
+  '!**/.eslintrc'
 ];
-const cleanHtml = [`${destinationCss}/**/*.html`];
-
-/**
- * Log to console an Error
- *
- * @param  {error} err error thrown during gulp task
- */
-function logError(err) {
-  if (err) gutil.log(gutil.colors.red('[Error]'), err.toString());
-}
+const cleanJs = [`${destinationJs}`];
+const cleanCss = [`${destinationCss}`];
+const cleanHtml = [
+  `${destinationHtml}/**/*.html`,
+  `${destinationHtml}/partials`
+];
+const isProduction = (process.env.NODE_ENV === 'production');
+const pumpPromise = (streams) => {
+  return new Promise((resolve, reject) => {
+    pump(streams, (err) => {
+      if (err) {
+        gutil.log(gutil.colors.red('[Error]'), err.toString());
+        reject(err);
+      } else resolve();
+    });
+  });
+};
 
 // validate js files
-gulp.task('eslint', () => {
-  pump([
-    gulp.src(esLintJs),
-    plugins.eslint(),
-    plugins.eslint.format('stylish'),
-    plugins.eslint.results((results) => {
-      if (results.errorCount > 0) {
-        gutil.log(gutil.colors.red('End in eslint task due to error(s)'));
-        process.exit(1);
-      }
-    }),
-  ], logError);
-});
+gulp.task('lint', () => pumpPromise([
+  gulp.src(lintJs),
+  plugins.eslint(),
+  plugins.eslint.format('stylish'),
+  plugins.eslint.failAfterError(),
+]));
 
-// remove js libs
-gulp.task('clean-js', () => {
-  del(cleanJs)
-    .catch(logError);
-});
+// removes js
+gulp.task('clean-js', () => del(cleanJs));
 
-// remove css libs
-gulp.task('clean-css', () => {
-  del(cleanCss)
-    .catch(logError);
-});
+// removes css
+gulp.task('clean-css', () => del(cleanCss));
 
-// remove css libs
-gulp.task('clean-html', () => {
-  del(cleanHtml)
-    .catch(logError);
-});
+// removes html
+gulp.task('clean-html', () => del(cleanHtml));
 
-// remove js and css lib from public
-gulp.task('clean', ['clean-js', 'clean-css', 'clean-html']);
+// removes all static files
+gulp.task('clean', (callback) => runSequence('clean-js', 'clean-css', 'clean-html', callback));
 
-// expose js lib in public
-gulp.task('copy-js-lib', ['clean-js'], () => {
-  pump([
-    gulp.src(libJs),
-    plugins.concat(finalLibJs),
-    gulp.dest(destinationJs),
-  ], logError);
-});
+// creates css from sass files and reload in dev mode
+gulp.task('css', () => pumpPromise([
+  gulp.src(sourceCss),
+  plugins.sass(),
+  plugins.cleanCss(),
+  gulp.dest(destinationCss),
+  plugins.if(!isProduction, plugins.connect.reload()),
+]));
 
-gulp.task('sass', () => {
-  pump([
-    gulp.src(sourceCss),
-    plugins.sass(),
-    gulp.dest(destinationCss),
-    plugins.connect.reload(),
-  ], logError);
-});
+// copy and minifies if necessary js files and reload in dev mode
+gulp.task('js', () => pumpPromise([
+  gulp.src((isProduction ? sourceJs : localJs)),
+  plugins.concat(finalJs),
+  gulp.dest(destinationJs),
+  plugins.if(isProduction, uglify({ mangle: false })),
+  plugins.if(isProduction, plugins.rename({ suffix: '.min' })),
+  plugins.if(isProduction, gulp.dest(destinationJs)),
+  plugins.if(!isProduction, plugins.connect.reload()),
+]));
 
-// minification of client js
-gulp.task('uglify', () => {
-  pump([
-    gulp.src(sourceJs),
-    plugins.concat(finalJs),
-    gulp.dest(destinationJs),
-    uglify({
-      mangle: {
-        toplevel: true,
-      },
-    }),
-    gulp.dest(destinationJs),
-    plugins.connect.reload(),
-  ], logError);
-});
+// copy and replaces in html js import if production mode and reload in dev mode
+gulp.task('html', () => pumpPromise([
+  gulp.src(sourceHtml),
+  plugins.if(isProduction, plugins.htmlReplace({ js: '/js/frontend.core.min.js' })),
+  gulp.dest(destinationHtml),
+  plugins.if(!isProduction, plugins.connect.reload()),
+]));
 
-gulp.task('jsdoc', plugins.shell.task(
-  'node node_modules/jsdoc/jsdoc.js ' +
-  '-c node_modules/angular-jsdoc/common/conf.json ' +
-  '-t node_modules/angular-jsdoc/default ' +
-  '-d doc ' +
-  './README.md ' +
-  '-r src'
-));
-
+// creates connect server for dev mode
 gulp.task('connect', () => {
-  plugins.connect.server({
-    root: 'dist',
-    livereload: true,
-    open: {
-      browser: 'chrome'
-    }
-  });
-});
-
-// Creates an index page for development tests
-gulp.task('init-html', () => {
-  try {
-    fs.writeFileSync(indexHtml, `
-      <!DOCTYPE html>
-      <html ng-app="frontend.core">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Frontend core module dev page</title>
-        <link rel="stylesheet" href="style.css">
-      </head>
-      <body>
-        <auth-dialog></auth-dialog>
-        <div ui-view />
-        <script src="lib.min.js"></script>
-        <script src="frontend.core.js"></script>
-      </body>
-      </html>`);
-  } catch (err) {
-    logError(err);
+  if (!isProduction) {
+    plugins.connect.server({
+      root: ['web', 'node_modules'],
+      livereload: true,
+    });
   }
-});
-
-// Livereload du HTML
-gulp.task('html', () => {
-  pump([
-    gulp.src(indexHtml),
-    plugins.connect.reload(),
-  ], logError);
 });
 
 // watch files for reload in dev mode
 gulp.task('watch', () => {
-  gulp.watch(sourceCss, ['sass']);
-  gulp.watch(sourceJs, ['uglify']);
-  gulp.watch(indexHtml, ['html']);
+  if (!isProduction) {
+    gulp.watch(sourceCss, ['css']);
+    gulp.watch(sourceJs, ['js']);
+    gulp.watch(sourceHtml, ['html']);
+  }
 });
 
-// default task (production)
-gulp.task('default', ['copy-js-lib', 'init-html', 'html', 'sass', 'uglify', 'connect', 'watch'])
-gulp.task('postinstall', ['sass', 'uglify']);
+gulp.task('default', (callback) => runSequence('clean', ['html', 'css', 'js'], 'connect', 'watch', callback));
+gulp.task('postinstall', ['default']);
